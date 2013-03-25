436e2e755dc012c730418bf8bc1988693e67a47d
Jose <josecriane@gmail.com>
2013-03-26 00:53:15 +0100

    Adds a class to securize parameters received
    The new class have a method to compare parameters with a regular expresion.

---
+++ server/backend/secureVars.php
@@ -0,0 +1,45 @@
+<?php
+/* Usage:
+$secureVars = new secureVars();
+ 
+ */
+
+//Defined regexps (you can add your own ones).
+define('REG_DIGIT_UNSIGNED', '^[[:digit:]]+$');
+define('REG_WORD'          , '^[[:alpha:]]+$');
+
+error_reporting(0); //Comment before deploying
+
+final class secureVars{
+  private $tmp;
+
+  //Check if the variable is set.
+  private function cisSet(&$field){
+    if(isset($_REQUEST[$field]))
+      return true;
+    else
+      throw new Exception("The $field field is empty.");
+  }
+  
+  //Set $tmp and remove threatening characters.
+  private function removeCharsThreats(&$field){
+    $this->tmp = trim($_REQUEST[$field]);
+    $this->tmp = htmlspecialchars($_REQUEST[$field], ENT_QUOTES, 'UTF-8', false); 
+  }
+
+  /* Checks if the value respects the defined regexp,
+  and if its length is not superior than the given maxlength. */
+  public function secText($field, $maxlength, $regexp){
+    if($this->cisSet($field)){
+      $this->removeCharsThreats($field);
+      if(!mb_ereg($regexp, $this->tmp))
+        throw new Exception("Unallowed characters in $field field.");
+      elseif(mb_strlen($this->tmp, 'UTF-8') > $maxlength)
+        throw new Exception("Too long string length for $field field.");
+      else
+        return $this->tmp;
+    }
+  }
+}
+
+?>
