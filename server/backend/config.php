4769f44f163ee656f0cbaa5de30d159a3d4ded86
Jose <josecriane@gmail.com>
2013-03-27 13:28:04 +0100

    Change a simple config.php

--- server/backend/config.php
+++ server/backend/config.php
@@ -1,11 +1,11 @@
 <?php
 
-  # Database config
-	define('DB_SERVER'        , 'localhost' );
-	define('DB_NAME'          , 'db_cookingcheats');
-	define('DB_USER'          , 'root');
-	define('DB_PASS'          , '###########');
+    # Database config
+    define('DB_SERVER'        , 'localhost' );
+    define('DB_NAME'          , 'db_cookingcheats');
+    define('DB_USER'          , 'root');
+    define('DB_PASS'          , '###########');
 
-	#Others configs
+    #Others configs
 
 ?>
