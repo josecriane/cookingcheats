06aa362f16f544241158a365a693ff9e0b481519
Jose <josecriane@gmail.com>
2013-04-22 18:37:34 +0200

    Changes view to recipe average.
    Now average show forks instead of numbers.
    Closes #26

--- server/API/config.php
+++ server/API/config.php
@@ -4,7 +4,7 @@
     define('DB_SERVER'        , 'localhost' );
     define('DB_NAME'          , 'db_cookingcheats');
     define('DB_USER'          , 'root');
-    define('DB_PASS'          , '##########');
+    define('DB_PASS'          , '#########');
 
     #Others configs
 
--- server/js/utils.js
+++ server/js/utils.js
@@ -21,10 +21,20 @@ var set_text = function (node, text){
 var init_recipe = function (Y, recipe){
   set_text(Y.one('#rec_name'), recipe.get('RecName'));
   set_text(Y.one('#difficulty_name'), recipe.get('Difficulty'));
-  set_text(Y.one('#average_name'), '5');
+  //set_text(Y.one('#average_name'), '5');
+  change_number_to_image(Y.one('#average_name'), "fork.png", 2)
   set_text(Y.one('#diners_name'), recipe.get('NumDiners'));
   set_text(Y.one('#time_name'), recipe.get('Time'));
   set_text(Y.one('#text'), recipe.get('RecDescription'));
+}
+
+var change_number_to_image = function (node, image, num) {
+  var img_tag = "<img src='./res/" + image + "' alt=" + image + " />";
+  var text = "";
+  for (var i = 0; i < num; i++){
+    text += img_tag;
+  }
+  set_text(node, text);
 }
 
 /*Search page functions */
